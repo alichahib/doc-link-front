a7c8e20617fa404a925797a19ae2282843bfb334
CHAHIB Ali <ali.chahib@softeam.fr>
2024-03-22 03:32:16 +0100

    add public and protected routes to use in A component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import {  Route, Routes } from 'react-router-dom';
 import Login from './pages/Login'; // Import the Login component
 import HomePage from './pages/HomePage';
 import Register from './pages/Register';
+import ProtectedRoute from './component/ProtectedRoute';
+import PublicRoute from './component/PublicRoute';
 //import Header from './component/Header';
 
 const App = () => {
   return (
     
       <Routes>
-          <Route path="/login" element={<Login/>} />
-          <Route path="/" element={<HomePage/>} />
-          <Route path="/register" element={<Register/>} />
+
+<Route path="/" element={<ProtectedRoute><HomePage/></ProtectedRoute>} />
+<PublicRoute><Route path="/login" element={<Login/>} /></PublicRoute>
+<PublicRoute><Route path="/register" element={<Register/>} /></PublicRoute>
+          
+          
       </Routes>
   );
 };
